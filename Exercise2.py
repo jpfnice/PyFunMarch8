26b6487129b866b71e53e67ce3fff8fdd6c85589
jpfnice <jpfnice@gmail.com>
2023-03-08 15:33:50 +0100

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,23 @@
+"""
+Exercise 2:
+    
+Step 0: 
+    Create an empty list "numbers"
+    
+Step 1: 
+    Retrieve numbers one by one using the input() function and a loop.
+    When the string "stop" will be provided it indicates the last number has    
+    been given.
+    Each number given will be stored into the list numbers.
+    
+Step 2:
+    Print the list numbers
+    
+Step 3:
+    Compute the minimum, the maximum and the average of the numbers stored in the list
+Step 4:
+    Compute the "truncated mean" of the numbers. The "truncated mean" takes into account all numbers except the smallest and largest ones:
+    Ex: [1,2,1,4,5,2,3,6,6,4,3]
+    -> [2,4,5,2,3,4,3] -> compute the mean
+    
+"""
